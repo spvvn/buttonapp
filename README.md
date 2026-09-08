39763bc6bd836d486510ae589610af712fc688fb
spvvn <connorjt223@gmail.com>
2026-09-08 15:40:38 -0500

    Create minimal Flutter button app

---
+++ README.md
@@ -0,0 +1,12 @@
+# Button App
+
+A minimal Flutter application with one button. Pressing the button intentionally has no effect.
+
+## Run
+
+After installing the Flutter SDK, run:
+
+```powershell
+flutter pub get
+flutter run
+```
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ButtonApp());
+}
+
+class ButtonApp extends StatelessWidget {
+  const ButtonApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {},
+            child: const Text('Press me'),
+          ),
+        ),
+      ),
+    );
+  }
+}
